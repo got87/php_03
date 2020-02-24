556d3c153405ff06bcd87d343b3c365005b02b5f
G Trifonov <52897194+got87@users.noreply.github.com>
2020-02-24 15:52:31 +0400

    Add files via upload
    Корректировки выполнены после замечаний от Andrew Cherabaev от 22.02.2020

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
 <?php
 
+header('Content-Type: text/event-stream');
+	
+// Определяет валидность слова
+function member($wrd)
+{
+  return empty($wrd) ? 0 : 1;
+}
   $txt = "Ехал Грека через реку. Видит Грека - в реке рак.
   Сунул Грека руку в реку. Рак за руку Греку цап!
-
   Карл украл у Клары кораллы.
   А Клара украла у Карла кларнет.
-
   Без труда и пруд-то не найдешь.
-
   Одна голова - хорошо. Две - Российский герб. А три - Змей Горыныч.
-
   Алкоголь в малых дозах полезен в любых количествах.
-
   Есть только миг между прошлым и будущем, именно он называется present continuous.";
 
-  $punkt= ['.', ',', '!', '?', '-', ':', ';', '\r', '\n', '\t'];
+  $punkt= ['.', ',', '!', '?', '-', ':', ';', PHP_EOL];
 
-  echo 'Исходный текст:<br>';
-  var_dump($txt);
-  echo '<br>';
+  echo 'Исходный текст:'.PHP_EOL;
+  echo $txt.PHP_EOL;
   
 
   // Всю имеющуюся пунктуацию и служебные символы сведем к пробелам
@@ -30,34 +31,30 @@
   // Выделим слова
   $words = explode(" ", $txt);
 
-  foreach ($words as $i => $v) {
-
-    $words[$i] = trim($v);
-	// Если слово пустое - исключим из массива
-    if (empty($words[$i])) {
-      unset($words[$i]);
-	}
+  foreach ($words as $i)
+  {
+    $words[$i] = trim($words[$i]);
   }
+
+  // Если слово пустое - исключим из массива
+  $words = array_filter($words, "member");
 
   // Сформируем словарь (массив уникальных слов)
   $dict = array_unique ($words, SORT_LOCALE_STRING);
-
-  echo '<table>';
-  echo '<tr> <th> № </th> <th> Слово </th> <th> Количество </tr> </tr>';
-
-  $num = 1;
+  
   foreach ($dict as $i => $v)
   {
 	$cnt_rep = 0; // Счетчик повторяющихся слов в тексте
-    foreach ($words as $j => $w) if ($v==$w) $cnt_rep++;
-    echo "<tr> <td> $num </td> <td> $v </td> <td> $cnt_rep </td> </tr>";
-	$num++;
+    foreach ($words as $j => $w) 
+	{
+	  if ($v==$w)
+      {
+        $cnt_rep++;
+	  }
+	}
+    echo "$v:$cnt_rep".PHP_EOL;
   }
 
   $cnt_wrd = count($words);
-
-  echo "<tr> <td colspan='2'> Всего слов в тексте </td> <td> $cnt_wrd </td>  </tr>";
-  echo '</table>';
-
-
+  echo "Всего слов:$cnt_wrd".PHP_EOL;
 ?>
